dac0a7056b97a9c989dadc8e4fc43993e2f3a632
Ben Hong <ben@bencodezen.io>
2022-11-01 16:24:50 -0500

    feature: add layout for atla-forum

---
+++ atla-forum/src/components/BaseLayout.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="grid">
+    <div>
+      <slot name="one" />
+    </div>
+    <div>
+      <slot name="two" />
+    </div>
+  </div>
+</template>
+
+<style>
+.grid {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  border: 5px solid papayawhip;
+}
+</style>
