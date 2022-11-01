a52eb959cfec2bfddc17a94a1789318d43a7d674
Ben Hong <ben@bencodezen.io>
2022-11-01 15:37:37 -0500

    feature: add favorite card in atla

---
+++ atla-forum/src/components/CharacterCard.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  props: {
+    character: {
+      type: Object,
+      required: true,
+    },
+  },
+  emits: ["favorite"],
+  methods: {
+    favoriteCharacter() {
+      this.$emit("favorite", this.character);
+    },
+  },
+};
+</script>
+
+<template>
+  <div>
+    <p>{{ character.name }}</p>
+    <button @click="favoriteCharacter">⭐ Favorite</button>
+  </div>
+</template>
