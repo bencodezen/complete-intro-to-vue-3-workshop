2451ceec52dd6ebfc20ce365bb8298d2664b2db1
Ben Hong <ben@bencodezen.io>
2022-11-01 16:24:50 -0500

    feature: add layout for atla-forum

--- atla-forum/src/App.vue
+++ atla-forum/src/App.vue
@@ -1,9 +1,11 @@
 <script>
+import BaseLayout from "./components/BaseLayout.vue";
 import BenderStatistics from "./components/BenderStatistics.vue";
 import CharacterCard from "./components/CharacterCard.vue";
 
 export default {
   components: {
+    BaseLayout,
     BenderStatistics,
     CharacterCard,
   },
@@ -45,6 +47,26 @@ export default {
 </script>
 
 <template>
+  <BaseLayout>
+    <template v-slot:two>
+      <h2>New Character</h2>
+      <pre>{{ newCharacter }}</pre>
+      <label for="character-name">Name</label>
+      <input
+        type="text"
+        v-model="newCharacter.name"
+        @keyup.enter="addNewCharacter"
+      />
+      <p>
+        <span
+          v-for="(character, index) in characterList"
+          :key="`comma-list-character-${index}`"
+          >{{ character.name
+          }}{{ index === characterList.length - 1 ? "" : ", " }}
+        </span>
+      </p>
+    </template>
+  </BaseLayout>
   <BenderStatistics :characters="characterList" />
   <h2>Characters</h2>
   <p v-if="characterList.length === 0">There are no characters</p>
@@ -67,19 +89,4 @@ export default {
     </li>
   </ul>
   <p v-else>No favorite characters yet!</p>
-  <h2>New Character</h2>
-  <pre>{{ newCharacter }}</pre>
-  <label for="character-name">Name</label>
-  <input
-    type="text"
-    v-model="newCharacter.name"
-    @keyup.enter="addNewCharacter"
-  />
-  <p>
-    <span
-      v-for="(character, index) in characterList"
-      :key="`comma-list-character-${index}`"
-      >{{ character.name }}{{ index === characterList.length - 1 ? "" : ", " }}
-    </span>
-  </p>
 </template>
