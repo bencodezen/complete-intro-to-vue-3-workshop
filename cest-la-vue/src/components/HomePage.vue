a4cb22ac61c560a7a02bbb0d911ad04eba3abe1b
Ben Hong <ben@bencodezen.io>
2022-11-02 07:35:34 -0500

    feature: add c'est la vue app starting point

---
+++ cest-la-vue/src/components/HomePage.vue
@@ -0,0 +1,43 @@
+<script></script>
+
+<template>
+  <main>
+    <h1>Welcome to <br />C'est La Vue</h1>
+    <p>
+      This is a place to manage various things: todos, users, posts, etc.
+      Whatever your mind desires!
+    </p>
+  </main>
+</template>
+
+<style>
+main {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  max-width: 320px;
+  margin: 0 auto;
+}
+
+main h1 {
+  margin-top: 10vh;
+  margin-bottom: 20px;
+}
+
+label {
+  margin-bottom: 5px;
+}
+
+input[type="email"] {
+  padding: 0.5rem;
+  margin-bottom: 30px;
+}
+
+button {
+  border: 1px solid green;
+  padding: 10px;
+  color: green;
+  background-color: rgb(213, 255, 213);
+  cursor: pointer;
+}
+</style>
