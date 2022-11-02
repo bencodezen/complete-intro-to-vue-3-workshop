e42c58d601aeaa2d0d20100054151fd5c8870425
Ben Hong <ben@bencodezen.io>
2022-11-02 13:37:41 -0500

    feature: add solution for composables

--- cest-la-vue/src/components/HomePage.vue
+++ cest-la-vue/src/components/HomePage.vue
@@ -1,4 +1,11 @@
-<script></script>
+<script setup>
+import { userList } from "../composables/useUserStore";
+import { computed } from "vue";
+
+const shortUserList = computed(() => {
+  return userList.value.splice(0, 5);
+});
+</script>
 
 <template>
   <main>
@@ -7,6 +14,12 @@
       This is a place to manage various things: todos, users, posts, etc.
       Whatever your mind desires!
     </p>
+    <hr />
+    <ul>
+      <li v-for="user in shortUserList">
+        {{ user.name }}
+      </li>
+    </ul>
   </main>
 </template>
 
--- cest-la-vue/src/components/UserPage.vue
+++ cest-la-vue/src/components/UserPage.vue
@@ -1,6 +1,7 @@
 <script setup>
 import UserCard from "./UserCard.vue";
-import { defineProps, reactive } from "vue";
+import { defineProps } from "vue";
+import { userList } from "../composables/useUserStore";
 
 defineProps({
   title: {
@@ -11,10 +12,6 @@ defineProps({
 
 defineEmits(["update-user-list"]);
 
-const state = reactive({
-  userList: [],
-});
-
 async function fetchUsers() {
   const response = await fetch(
     "https://jsonplaceholder.typicode.com/users"
@@ -23,7 +20,7 @@ async function fetchUsers() {
   return response;
 }
 
-state.userList = await fetchUsers();
+userList.value = await fetchUsers();
 </script>
 
 <template>
@@ -31,7 +28,7 @@ state.userList = await fetchUsers();
     <h1>{{ title }}</h1>
     <ul>
       <UserCard
-        v-for="user in state.userList"
+        v-for="user in userList"
         :user="user"
         :key="`user-${user.id}`"
       />
