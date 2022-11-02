d52bc8178298f7abeec0bdc5723fd0d136bd2f41
Ben Hong <ben@bencodezen.io>
2022-11-02 14:05:38 -0500

    refactor: improve styling solution for cest la vue

--- cest-la-vue/src/components/HomePage.vue
+++ cest-la-vue/src/components/HomePage.vue
@@ -23,34 +23,4 @@ const shortUserList = computed(() => {
   </main>
 </template>
 
-<style>
-main {
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-  max-width: 320px;
-  margin: 0 auto;
-}
-
-main h1 {
-  margin-top: 10vh;
-  margin-bottom: 20px;
-}
-
-label {
-  margin-bottom: 5px;
-}
-
-input[type="email"] {
-  padding: 0.5rem;
-  margin-bottom: 30px;
-}
-
-button {
-  border: 1px solid green;
-  padding: 10px;
-  color: green;
-  background-color: rgb(213, 255, 213);
-  cursor: pointer;
-}
-</style>
+<style></style>
--- cest-la-vue/src/components/UserCard.vue
+++ cest-la-vue/src/components/UserCard.vue
@@ -10,10 +10,10 @@ export default {
 </script>
 
 <template>
-  <li class="user-card">{{ user.name }}: {{ user.website }}</li>
+  <li :class="$style['user-card']">{{ user.name }}: {{ user.website }}</li>
 </template>
 
-<style>
+<style module>
 .user-card {
   border: 2px solid #222;
   padding: 10px;
