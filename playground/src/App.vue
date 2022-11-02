9b0dbf55b1360936f3afc3040d2805575ddc132c
Ben Hong <ben@bencodezen.io>
2022-11-02 14:53:41 -0500

    feature: add programmatic navigation

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -1,9 +1,20 @@
 <script setup>
-import BaseCounter from "./components/base-counter.vue";
-import UserCard from "./components/user-card.vue";
-import { ref } from "vue";
+import BaseCounter from "@/components/base-counter.vue";
+import UserCard from "@/components/user-card.vue";
+import { useCount } from "@/composables/countStore";
+import { ref, watch } from "vue";
+import { useRouter } from "vue-router";
 
 const colorPreference = ref("white");
+const countStore = useCount();
+const router = useRouter();
+
+watch(countStore.globalCount, (val) => {
+  if (val > 1200) {
+    // navigate user to Pokedex
+    router.push("/pokedex");
+  }
+});
 </script>
 
 <template>
@@ -12,6 +23,7 @@ const colorPreference = ref("white");
       <router-link to="/">Home</router-link>
       <router-link to="/pokedex">Pokedex</router-link>
     </nav>
+    <h2>{{ countStore.globalCount }}</h2>
     <router-view />
     <h2>{{ colorPreference }}</h2>
     <input type="color" v-model="colorPreference" />
