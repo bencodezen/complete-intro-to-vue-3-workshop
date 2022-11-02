ac2f5b957f8d34ab7799f0d5de641ffbbcfebf71
Ben Hong <ben@bencodezen.io>
2022-11-02 10:06:39 -0500

    feature: add pokedex fetch api call

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -1,7 +1,27 @@
-<script></script>
+<script>
+// https://pokeapi.co/api/v2/pokemon?limit=151
+
+export default {
+  data: () => ({
+    pokedex: [1, 2, 3],
+  }),
+  methods: {
+    async fetchPokemon() {
+      this.pokedex = await fetch(
+        "https://pokeapi.co/api/v2/pokemon?limit=151"
+      ).then((response) => response.json());
+    },
+  },
+  created() {
+    this.fetchPokemon();
+  },
+};
+</script>
 
 <template>
   <h1>New App</h1>
+  <pre>{{ pokedex }}</pre>
+  <button @click="fetchPokemon">Fetch Pokemon</button>
 </template>
 
 <style></style>
