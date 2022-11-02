e01abafbcaa7db076ea450a8600e7e4e22b4c4a3
Ben Hong <ben@bencodezen.io>
2022-11-02 07:35:34 -0500

    feature: add c'est la vue app starting point

---
+++ cest-la-vue/src/App.vue
@@ -0,0 +1,77 @@
+<script>
+import HomePage from "./components/HomePage.vue";
+import LoginPage from "./components/LoginPage.vue";
+
+export default {
+  components: {
+    HomePage,
+    LoginPage,
+  },
+  data: () => ({
+    currentPage: "Home",
+  }),
+  methods: {
+    showHomePage() {
+      this.currentPage = "Home";
+    },
+    showLoginPage() {
+      this.currentPage = "Login";
+    },
+  },
+};
+</script>
+
+<template>
+  <header class="header">
+    <span class="logo">
+      <img src="@/assets/vue-heart.png" width="30" />C'est La Vue
+    </span>
+    <nav class="nav">
+      <a href="#" @click.prevent="showHomePage">Home</a>
+      <a href="#" @click.prevent="showLoginPage">Login</a>
+    </nav>
+  </header>
+  <HomePage v-if="currentPage === 'Home'" />
+  <LoginPage v-else />
+</template>
+
+<style>
+* {
+  box-sizing: border-box;
+  font-family: "Inter", sans-serif;
+  margin: 0;
+  padding: 0;
+}
+
+.header {
+  display: flex;
+  justify-content: space-between;
+  padding: 0.5rem 1rem;
+  border-bottom: 1px solid #ccc;
+}
+
+span.logo {
+  display: flex;
+  align-items: center;
+  font-weight: bold;
+  font-size: 1.2rem;
+}
+
+span.logo img {
+  margin-right: 8px;
+}
+
+.nav {
+  display: flex;
+  align-items: center;
+}
+
+.nav a {
+  padding: 0.5rem;
+  font-size: 0.9rem;
+}
+
+.nav a:last-child {
+  padding-right: 0;
+}
+</style>
