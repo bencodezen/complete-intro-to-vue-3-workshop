b74f38b50349e9e92a0b85f2ad3d3f59949a1760
Ben Hong <ben@bencodezen.io>
2022-11-02 11:24:40 -0500

    feature: add composition api example in pokedex

--- playground/src/components/pokedex.vue
+++ playground/src/components/pokedex.vue
@@ -1,18 +1,41 @@
 <script>
 // https://pokeapi.co/api/v2/pokemon?limit=151
+import { computed, ref, reactive } from "vue";
 
 export default {
   async setup() {
-    const regionName = "kanto";
+    const regionName = ref("Kanto");
+
+    const state = reactive({
+      elementType: "lightning",
+    });
+
+    console.log(regionName);
+    console.log(state);
+
+    const elementTypeAllCaps = computed(() => {
+      return state.elementType.toUpperCase();
+    });
 
     const pokedex = await fetch(
       "https://pokeapi.co/api/v2/pokemon?limit=151"
     ).then((response) => response.json());
 
     return {
+      elementTypeAllCaps,
       pokedex,
       regionName,
     };
+  },
+  computed: {
+    regionNameLowerCase() {
+      return this.regionName.toLowerCase();
+    },
+  },
+  methods: {
+    changeRegionName() {
+      this.regionName = "Hoenn";
+    },
   },
   created() {
     console.log(this.regionName);
@@ -22,5 +45,9 @@ export default {
 </script>
 
 <template>
+  <h2>{{ regionName }}</h2>
+  <h3>{{ elementTypeAllCaps }}</h3>
+  <h3>{{ regionNameLowerCase }}</h3>
+  <button @click="changeRegionName">Change Region Name</button>
   <pre>{{ pokedex }}</pre>
 </template>
